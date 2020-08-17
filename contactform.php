3df7859b52ee72f82a49d62364e965221f21e7fb
Dan <dan@dans-mbp.home>
2020-08-17 12:28:44 +0100

    phpmailer intergration

--- contactform.php
+++ contactform.php
@@ -1,22 +1,29 @@
+
+
 <?php
+require 'PHPMailerAutoload.php';
 
-if(isset($_POST['submit'])) {
-    $fName = $_POST['fName'];
-    $lName = $_POST['lName'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
+$mail = new PHPMailer;
 
-    $mailTo = "daniel.goodfellow@yahoo.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$fName.".\n\n".$message;
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yahoo.com';                     // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'daniel.goodfellow@yahoo.com';   // SMTP username
+$mail->Password = 'Rawg3ts3!';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
 
+$mail->From = 'daniel.goodfellow';
+$mail->FromName = 'Mailer';
+$mail->addAddress('Daniel.goodfellow@sky.com');                 // Add a recipient
 
-    mail(
-        $mailTo,
-        $txt,
-        $headers
-    );
+$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 
-    header("Location: contact.php?mailsend");
+$mail->Subject = 'Hello';
+$mail->Body    = 'Testing some Mailgun awesomness';
 
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
 }
