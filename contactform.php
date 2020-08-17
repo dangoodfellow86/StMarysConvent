f2f3efd187f199af7d054d620c6d4e75eb85b28b
Dan <dan@dans-mbp.home>
2020-08-17 12:47:51 +0100

    Debugging

--- contactform.php
+++ contactform.php
@@ -1,29 +1,34 @@
 
 
 <?php
-require 'PHPMailerAutoload.php';
+    if(isset($_POST['submit'])){
+    require 'phpmailer/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+    $mail = new PHPMailer;
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yahoo.com';                     // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'daniel.goodfellow@yahoo.com';   // SMTP username
-$mail->Password = 'Rawg3ts3!';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
+                                          // Set mailer to use SMTP
+    $mail->Host = 'smtp.yahoo.com';
+    $mail->Port = 587;                     // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'daniel.goodfellow@yahoo.com';   // SMTP username
+    $mail->Password = 'Rawg3ts3!';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
 
-$mail->From = 'daniel.goodfellow';
-$mail->FromName = 'Mailer';
-$mail->addAddress('Daniel.goodfellow@sky.com');                 // Add a recipient
+    $mail->setFrom = 'daniel.goodfellow';
+    $mail->addAddress('Daniel.goodfellow@sky.com');
+                    // Add a recipient
+    
+    
+      
 
-$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 
-$mail->Subject = 'Hello';
-$mail->Body    = 'Testing some Mailgun awesomness';
+    $mail->Subject = 'Hello This is a test';
+    $mail->Body    = 'Testing some Mailgun awesomness';
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
+    }
