27cd719c002bee55ee84c2c103e671aabaa579e7
Dan <dan@dans-mbp.home>
2020-08-17 12:57:00 +0100

    update password

--- contactform.php
+++ contactform.php
@@ -11,7 +11,7 @@
     $mail->Port = 465;                     // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'daniel.goodfellow@yahoo.com';   // SMTP username
-    $mail->Password = 'Rawg3ts3!';                           // SMTP password
+    $mail->Password = 'rawgets3';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
 
     $mail->setFrom = 'daniel.goodfellow';
