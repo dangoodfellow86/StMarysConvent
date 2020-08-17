ad5992f9da7ba8de8e4c4ade6d6e298b816e3a0f
Dan <dan@dans-mbp.home>
2020-08-17 13:43:52 +0100

    api removal

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@
     use Mailgun\Mailgun;
     
     # Instantiate the client.
-    $mgClient = Mailgun::create('a232d415d2cdddf19942fcd42d11005b-203ef6d0-1e70ea81', 'https://api.mailgun.net/v3/sandbox9da3b219d1ef41258a670b124f821fc4.mailgun.org');
+    $mgClient = Mailgun::create();
     $domain = "sandbox9da3b219d1ef41258a670b124f821fc4.mailgun.org";
     $params = array(
     'from'    => 'Excited User <YOU@sandbox9da3b219d1ef41258a670b124f821fc4.mailgun.org	>',
