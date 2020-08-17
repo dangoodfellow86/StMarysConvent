8955494347799634956a4dc3ffbfbaf202c106f0
Dan <dan@dans-mbp.home>
2020-08-17 13:53:35 +0100

    api update

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@
     use Mailgun\Mailgun;
     
     # Instantiate the client.
-    $mgClient = Mailgun::create();
+    $mgClient = Mailgun::create('key-3557d8cd64399fa5fe5b7ebf77c33c38', 'https://api.mailgun.net/v3/sandbox9da3b219d1ef41258a670b124f821fc4.mailgun.org');
     $domain = "sandbox9da3b219d1ef41258a670b124f821fc4.mailgun.org";
     $params = array(
     'from'    => 'Excited User <YOU@sandbox9da3b219d1ef41258a670b124f821fc4.mailgun.org	>',
